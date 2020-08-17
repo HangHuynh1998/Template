ddd25fb15a9a923df26a8af8ef339eb53e246c1d
HangHuynh1998 <huynhhang.24hdev@gmail.com>
2020-08-17 17:56:51 +0700

    Add remove button

--- client/src/components/Boxes/Boxes.js
+++ client/src/components/Boxes/Boxes.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import Button from '../Button/Button';
 import Box from './Box/Box';
 import "./Boxes.scss"
+import Axios from 'axios';
 class Boxes extends Component {
     state={
-        show:false
+        show:false,
+        input:null,
     }
     HandlerShow=()=>{
         this.setState({show:!this.state.show})
+    }
+    HandlerInput=(event)=>{
+        event.preventDefault();
+        console.log(event.target.value);
+        this.setState({input:event.target.value})
+    }
+    DeletePost=()=>{
+       console.log(this.state.input);
+       Axios.delete(`https://jsonplaceholder.typicode.com/comments/`+this.state.input)
+    .then(res => {
+      console.log("DeletePost",res);
+    })  
+    .catch(error => console.log(error));
     }
     render() {
         console.log(this.state.show);
@@ -15,6 +30,13 @@ class Boxes extends Component {
             <div className='Boxes'>
                 {this.state.show ?<Box/>:null}
                 <Button clicked={this.HandlerShow}>Pull Data</Button>
+                {this.state.show ?(
+                 <div>
+                 <input type='text' onChange={(e)=>this.HandlerInput(e)}/>
+                 <Button clicked={this.DeletePost}>Remove</Button>
+                 </div>
+               
+                ):null}
             </div>
         );
     }
